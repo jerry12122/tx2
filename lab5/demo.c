2dba6a36150516551c2e9a65a1fe0550f75e67dd
jerry12122 <a0975844268@gmail.com>
2021-05-31 12:52:49 +0800

    Signed-off-by: jerry12122 <a0975844268@gmail.com>

--- lab5/demo.c
+++ lab5/demo.c
@@ -2,38 +2,125 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
-#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+#include <linux/string.h>
+
 #define MAJOR_NUM 60
 #define MODULE_NAME "demo"
 
 static int iCount=0;
+static char userChar[100];
+
 static ssize_t drv_read(struct file *filp,char *buf,size_t count,loff_t *ppos){
     printk("Enter Read function\n");
+    printk("W_buf_size: %d \n",(int)count);
+    copy_from_user(&userChar,buf,count);
+    userChar[count - 1] = '\0';
+    printk("userChar(char): %s\n",userChar);
+    char gpio[10] = {0};
+    strncpy(gpio,userChar,3);
+    printk("gpio: %s length: %d\n",gpio,strlen(gpio));
+    
+    struct file *io;
+    loff_t pos = 0;
+    mm_segment_t old_fs;
+    old_fs = get_fs();
+    set_fs(get_ds());
+
+    //read-value
+    if(strcmp(gpio,"396")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio396/value",O_RDONLY,0);
+    }
+    if(strcmp(gpio,"397")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio397/value",O_RDONLY,0);
+    }
+    if(strcmp(gpio,"429")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio429/value",O_RDONLY,0);
+    }
+    if(strcmp(gpio,"398")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio398/value",O_RDONLY,0);
+    }
+
+    vfs_read(io,userChar,1,&pos);
+    //printk("ADADADADADADADDA): %s\n",buff);
+    copy_to_user(buf,&userChar,1);
+    filp_close(io,NULL);
+
     return count;
 }
-/*
-static ssize_t drv_wirite(struct file *filp,const char *buf,size_t count,loff_t *ppos){
-    printk("Enter write function\n");
-    printk("%d\n",iCount);
-    printk("W_buf_size: %d \n"(int)count);
 
-    copy_from_user(userChar,buf,count);
-    userChar[count - 1] = 0;
-
-    printk("userChar(chr): %s\n",userChar);
-    printk("userChar(int): %d\n",(int)sizeof(userChar));
-
-}*/
 static ssize_t drv_write(struct file *filp,const char *buf,size_t count,loff_t *ppos){
     printk("Enter Write function\n");
     printk("%d\n",iCount);
     printk("W_buf_size: %d \n",(int)count);
-    printk("buf: %s\n",buf);
+    copy_from_user(userChar,buf,count);
+    userChar[count - 1] = '\0';
+    printk("userChar(char): %s\n",userChar);
+    char gpio[10] = {0};
+    strncpy(gpio,userChar,3);
+    printk("gpio: %s length: %d\n",gpio,strlen(gpio));
+    struct file *io;
+    loff_t pos = 0;
+    mm_segment_t old_fs;
+    old_fs = get_fs();
+    set_fs(get_ds());
+    //export
+    io = filp_open("/sys/class/gpio/export",O_WRONLY,0);
+    vfs_write(io,gpio,strlen(gpio),&pos);
+    filp_close(io,NULL);
+    pos=0;
+    //set-dir
+    if(strcmp(gpio,"396")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio396/direction",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"397")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio397/direction",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"429")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio429/direction",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"398")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio398/direction",O_WRONLY,0);
+    }
+    vfs_write(io,"out",3,&pos);
+    filp_close(io,NULL);
+    pos=0;
+
+    //set-value
+    if(strcmp(gpio,"396")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio396/value",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"397")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio397/value",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"429")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio429/value",O_WRONLY,0);
+    }
+    if(strcmp(gpio,"398")==0)
+    {
+        io = filp_open("/sys/class/gpio/gpio398/value",O_WRONLY,0);
+    }
+    vfs_write(io,&userChar[count-2],1,&pos);
+    filp_close(io,NULL);
+    pos=0;
+
+    set_fs(old_fs);
     iCount++;
     return count;
 
 }
-long drv_ioctl(struct file *filp,unsigned int cmd,unsigned long arg)
+static long drv_ioctl(struct file *filp,unsigned int cmd,unsigned long arg)
 {
     printk("Enter I/O Control function\n");
     return 0;
--- lab5/test.c
+++ lab5/test.c
@@ -1,14 +1,88 @@
 #include <stdio.h>
-int main()
+#include <stdlib.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<iostream>
+#include<string>
+#include<map>
+
+using namespace std;
+void setGPIO(unsigned int gpio,string status)
 {
-    char buf[1024] = "Data Input 123456 hello world";
-    FILE *fp = fopen("/dev/demo","w+");
-    if(fp == NULL){
-        printf("can't open device!\n");
-        return 0;
+    int io;
+    io = open("/dev/demo",O_WRONLY);
+    if(io<0){
+        perror("gpio error");
+        return;
     }
-    fwrite(buf,sizeof(buf),1,fp);
-    fread(buf,sizeof(buf),1,fp);
-    fclose(fp);
+    char buf[10] = {0};
+    if(status == "on")
+    {
+        strcpy(buf,(to_string(gpio)+"1").c_str());
+    }
+    else
+    {
+        strcpy(buf,(to_string(gpio)+"0").c_str());
+    }
+    write(io,buf,5);
+    close(io);
+    return;
+}
+void readGPIO(unsigned int gpio)
+{
+    int io;
+    io = open("/dev/demo",O_RDONLY);
+    if(io<0){
+        perror("gpio error");
+        return;
+    }
+    char buf[10] = {0};
+    strcpy(buf,(to_string(gpio)).c_str());
+    read(io,buf,4);
+    cout << buf[0] << endl;
+    close(io);
+    return;
+}
+int main(int argc,char *argv[])
+{
+    
+    if(argc==3)
+    {
+        string a = argv[1];
+        string status = argv[2];
+        if(a=="LED1"){
+            setGPIO(396,status);
+        }
+        else if(a=="LED2"){
+            setGPIO(397,status);
+        }
+        else if(a=="LED3"){
+            setGPIO(429,status);
+        }
+        else if(a=="LED4"){
+            setGPIO(398,status);
+        }
+    }
+    else
+    {
+        
+        string a = argv[1];
+        cout << a << " Status: ";
+        if(a=="LED1"){
+            readGPIO(396);
+        }
+        else if(a=="LED2"){
+            readGPIO(397);
+        }
+        else if(a=="LED3"){
+            readGPIO(429);
+        }
+        else if(a=="LED4"){
+            readGPIO(398);
+        }
+    }
+
     return 0;
 }
